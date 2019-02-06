adf6b3dc7e801371ed68c21d1f6924577245d7c9
jinjuraju <44498620+jinjuraju@users.noreply.github.com>
2019-02-06 12:27:19 +0530

    Add files via upload

---
+++ greatestof3.c
@@ -0,0 +1,20 @@
+/*PROGRAM TO FIND GREATEST OF THREE NUMBERS
+Author:Jinju Raju
+Date:06/02/19*/
+#include<stdio.h>
+void main()
+{
+int number1,number2,number3,big;
+printf("enter the three integer numbers");
+scanf("%d%d%d",&number1,&number2,&number3);
+big=number1;
+if(big<number2)
+	{
+	big=number2;
+	}
+if(big<number3)
+	{
+	big=number3;
+	}
+printf("%d is greater than the three numbers %d,%d and%d",big,number1,number2,number3);
+}
