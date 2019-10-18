080f760ca16cd46ecf8038de6f25572b7fec023e
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2019-10-18 22:04:24 +0530

    Change in variable

--- greatestof3.c
+++ greatestof3.c
@@ -4,10 +4,10 @@ Date:06/02/19*/
 #include<stdio.h>
 void main()
 {
-int number1,number2,number3,big;
+int num1,number2,number3,big;
 printf("enter the three integer numbers");
-scanf("%d%d%d",&number1,&number2,&number3);
-big=number1;
+scanf("%d%d%d",&num1,&number2,&number3);
+big=num1;
 if(big<number2)
 	{
 	big=number2;
@@ -16,5 +16,5 @@ if(big<number3)
 	{
 	big=number3;
 	}
-printf("%d is greater than the three numbers %d,%d and%d",big,number1,number2,number3);
+printf("%d is greater than the three numbers %d,%d and%d",big,num1,number2,number3);
 }
