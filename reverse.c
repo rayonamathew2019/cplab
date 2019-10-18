bfbdacc5e86f1abbeec477467d7d9fdb74fefe69
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2019-10-18 22:09:57 +0530

    Change in variable

--- reverse.c
+++ reverse.c
@@ -4,15 +4,15 @@ DATE:13/02/19*/
 #include<stdio.h>
 void main()
 {
-	int number,t,reverse=0,copy,i;
+	int number,t,r=0,copy,i;
 	printf("enter the number to be reversed");
 	scanf("%d",&number);
 	copy=number;
 for(i=0;number>0;i++)
 	{
 	t=number%10;
-	reverse=(reverse*10)+t;
+	r=(r*10)+t;
 	number=number/10;
 	}
-printf("the reverse of the number %d is %d",copy,reverse);
+printf("the reverse of the number %d is %d",copy,r);
 }
