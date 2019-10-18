2d297198f5cacdcaa2e46ce6ebd02b8c46091e09
jiyamathai <44498560+jiyamathai@users.noreply.github.com>
2019-10-18 22:02:35 +0530

    Change in varuable l

--- area.c
+++ area.c
@@ -4,11 +4,11 @@ DATE:20/02/19*/
 #include<stdio.h>
 void main()
 {
-int length,breadth,area,perimeter;
+int l,breadth,area,perimeter;
 printf("enter the length and breadth");
-scanf("%d%d",&length,&breadth);
+scanf("%d%d",&l,&breadth);
 area=length*breadth;
-perimeter=2*(length+breadth);
+perimeter=2*(l+breadth);
 if (area>perimeter)
 	{
 	printf("area is greater than perimeter");
